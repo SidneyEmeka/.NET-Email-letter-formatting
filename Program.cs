fb750faafc61335e39fcd63e490ac43a2ec5d770
Sidswipe <sidswipexyz@gmail.com>
2024-03-11 16:51:57 +0100

    Data Formatting for an Email Template

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+
+Console.WriteLine($"Dear {customerName}");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}  Given your current volume, your potential profit would be {newProfit:C}.\n");
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = $"{currentProduct.PadRight(19)} {currentReturn:P2}   {currentProfit:C}\n{newProduct.PadRight(19)} {newReturn:P2}   {newProfit:C}";
+
+Console.WriteLine(comparisonMessage);
---
+++ README..md
@@ -0,0 +1,8 @@
+# Form Letter/E-mail 
+Creating an Email Template Using .NET formatters like;<br>
+:C - currency formatter<br>
+:N - number formatter<br>
+:P - Percentage formatter<br>
+.PadLeft(12) & .PadRight(12) - Pads a string from its left or to its right to make the number of its characters match the number in its argument as in this case 12 characters long. It can be overloaded to take another argument which specifies what the string will be padded with e.g .PadLeft(12, '*') , this indents the string with *s until it matches the supposed character length<br>
+
+## Data Formatting
